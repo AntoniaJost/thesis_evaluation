12b777d23d7ae8d42c6df5058ea7997f5707ea2a
AntoniaJost <antonia.jost@web.de>
2026-03-17 10:13:53 +0100

    improve documentation

--- run_eval.sh
+++ run_eval.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=GM_aw+gen
-#SBATCH --time=06:00:00
-#SBATCH --partition=gpu
+#SBATCH --job-name=stats_ALL
+#SBATCH --time=08:00:00
+#SBATCH --partition=compute
 #SBATCH --account=bk1450
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=12G
 #SBATCH --output=slurm_%x_%j.out
 #SBATCH --error=slurm_%x_%j.err
+#SBATCH --dependency=afterany:23506256
 
 source ~/.bashrc
 conda activate thesis_eval
 set -euo pipefail
 
-# if running for the first time and bias maps are wanted, calculate the corresponding csv files first! only needed once
-# python -m evaluation.range_summary
+# ==== RANGE SUMMARY =====
+# if running for the first time and bias maps are wanted, calculate the corresponding csv files first! only needed once (depending on the number of models you select, this will take approx. x hours (for 6 models))
+python -m evaluation.range_summary \
+  'range_summary.models_to_process=["free_run_control","free_run_prediction","forced_sst","forced_sst_2k","forced_sst_4k","archesweather"]' \
+  'range_summary.tag=_ALL'
+# options: ["free_run_control","free_run_prediction","forced_sst","forced_sst_2k","forced_sst_4k","archesweather"]
 
-# [hus, psl, siconc, ta, tas, tos, ua, uas, va, vas, wap, zg]
-# imporant: for safety best put everything into '', a separate line and DO NOT use spaces when listing arguments!
+# ===== CALC PLOTS =====
+# variable options: [hus, psl, siconc, ta, tas, tos, ua, uas, va, vas, wap, zg]
+# imporant: for safety best to put everything into '', a separate line and DO NOT use spaces when listing arguments!
 
 # ---- GLOBAL MEAN ----
-python -m evaluation.main \
-  run_plots='["global_mean"]' \
-  members='["member1","member2","member4","member5"]' \
-  'out.overwrite=true' \
-  plots.global_mean.models='["forced_sst","archesweather"]' \
+# python -m evaluation.main \
+#   run_plots='["global_mean"]' \
+#   members='["member1","member2","member4","member5"]' \
+#   'out.overwrite=true' \
+#   plots.global_mean.models='["forced_sst","archesweather"]' \
 
 # ---- BIAS MAPS ----
+# for 5 models at once, this can take a bit over 12h
 # python -m evaluation.main \
 #   'run_plots=["bias_map"]' \
 #   'out.overwrite=true' \
@@ -41,8 +48,8 @@ python -m evaluation.main \
 #   'plots.bias_map.models=["free_run_prediction"]' \
 #   'plots.bias_map.time.use_named=TSTP'
 
-
 # ---- SOI ----
+# runs within minutes for all models at once
 # python -m evaluation.main \
 #   'run_plots=["soi"]' \
 #   'out.overwrite=true' \
